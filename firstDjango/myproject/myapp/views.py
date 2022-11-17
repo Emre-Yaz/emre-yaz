0505d198d4b842c140784c431877e4d6dab11e8d
Emre-Yaz <ibrahimemreyaz@gmail.com>
2022-11-17 23:39:50 +0300

    Linking the URL's and adding a word counter.

--- firstDjango/myproject/myapp/views.py
+++ firstDjango/myproject/myapp/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'firstwebpage.html')
+    context = {
+        'name' : 'Emre',
+        'age' : 23,
+        'nationality' : 'Turkish',
+    }
+    return render(request, 'firstwebpage.html', context)
+
+def counter(request):
+    text = request.POST['text']
+    amountOfWords = len(text.split())
+    return render(request, 'counter.html', {'amount' : amountOfWords}) 
