7ad86b66b128f650707877c43006839bd0d06d1e
Emre-Yaz <ibrahimemreyaz@gmail.com>
2022-11-27 20:09:39 +0300

    Register-login-logout added.

--- firstDjango/myproject/myapp/urls.py
+++ firstDjango/myproject/myapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('counter', views.counter, name = 'counter'),
-    
+    path('register', views.register, name = 'register'),
+    path('login', views.login, name = 'login'),
+    path('logout', views.logout, name = 'logout'),
 ]
